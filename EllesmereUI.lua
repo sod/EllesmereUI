b41f7be2341ba4738296dbcfe00e93f18fc872d2
EllesmereGaming <ellesmeregaming@gmail.com>
2026-03-13 02:15:12 -0400

    Release v4.6.6

--- EllesmereUI.lua
+++ EllesmereUI.lua
@@ -5531,7 +5531,7 @@ end
 -------------------------------------------------------------------------------
 --  Slash commands
 -------------------------------------------------------------------------------
-EllesmereUI.VERSION = "4.6.5"
+EllesmereUI.VERSION = "4.6.6"
 
 -- Register this addon's version into a shared global table (taint-free at load time)
 if not _G._EUI_AddonVersions then _G._EUI_AddonVersions = {} end
